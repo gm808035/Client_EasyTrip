2f9b7196dcee9edf66a9f1fbd1ba97942bb5701f
a kadyrova <gm808035@gmail.com>
2020-05-06 18:13:01 +0600

    remove excess function

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
   import Search from './components/Search';
   import TripList from "./components/TripList";
   import PlaceList from "./components/PlaceList";
-  import User from "./views/User"
+
   // import Vuex from 'vuex'
   export default {
 
@@ -24,27 +24,7 @@
       Banner,
       Search
     },
-    computed : {
-      isLoggedIn : function(){ return this.$store.getters.isLoggedIn}
-    },
-    methods: {
-      logout: function () {
-        this.$store.dispatch('logout')
-          .then(() => {
-            this.$router.push('/login')
-          })
-      }
-    },
-    created: function () {
-      this.$http.interceptors.response.use(undefined, function (err) {
-        return new Promise(function (/*resolve, reject*/) {
-          if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
-            this.$store.dispatch("logout")
-          }
-          throw err;
-        });
-      });
-    }
+
   }
 </script>
 
